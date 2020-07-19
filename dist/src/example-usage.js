a69c02b59538f65f52d4c0390505da4563cf7b86
Rich Plastow <rich.plastow@easeltv.com>
2020-07-19 11:48:58 +0100

    Adds "files" array to ‘tsconfig.json’ for a cleaner ‘dist/’ folder

--- dist/src/example-usage.js
+++
@@ -1,8 +0,0 @@
-import "./example-usage.css";
-import { name, version } from "../package.json";
-import { CoAnMoPluginCli } from "./CoAnMoPluginCli";
-import { actions } from "./Actions/actions";
-var cli = new CoAnMoPluginCli(name, version, ".stdin", ".stdout", document);
-cli.addActions(actions);
-cli.focusOnInput();
-//# sourceMappingURL=example-usage.js.map
